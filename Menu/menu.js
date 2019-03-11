bb726b34bb22ec2da83356dcdcb07a6e0cbc8dfa
Unknown <seguragabriel@unbosque.edu.co>
2019-03-11 09:03:58 -0500

    EPOC 11 Mar (WIP)

---
+++ Menu/menu.js
@@ -0,0 +1,51 @@
+//Banner top
+$('#banner-home').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/EPOC-astrazeneca/images/assets/banner-home.jpg" alt="MIELOMA - LINFOMA: “Retos en diagnóstico y terapias novedosas”"/>');
+
+// Menu numero 1 -----
+$('#menu').html('<div> <nav> <div class="menu-toggle"> <i class="fa fa-bars"> </i> Menú </div><ul> <li class="home"> <a href="https://ubosquemoodle.unbosque.edu.co/course/view.php?id=4228"> <i class="fa fa-home"> </i> </a> </li><li class="dropdown timeit" data-start=" 2019-03-04 00:00"> <a href=""> <span>0. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285720">Carta bienvenida UEB </a> </li></ul> </li><li class="dropdown timeit" data-start=" 2019-03-04 00:00"> <a href=""> <span>1. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285744">Fisiopatología del Mieloma Múltiple </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285745">Formación de los plasmocitos o células plasmáticas </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285746">Definiciones </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285748">Evolución clonal en MM </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285749">Subdivisión del Mieloma Múltiple desde el punto de vista genético </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285750">Eventos secundarios </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285751">Microambiente </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285752">Conclusiones </a> </li><li role="presentation" class="divider"> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=285753">Bibliografía </a> </li></ul> </li><li class="dropdown timeit" data-start=" 2019-04-04 00:00"> <a href=""> <span>2. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">Test </a> </li><li role="presentation" class="divider"> </li><li> <a href="#">Test </a> </li></ul> </li><li class="dropdown timeit" data-start=" 2019-04-04 00:00"> <a href=""> <span>3. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">Test </a> </li><li role="presentation" class="divider"> </li><li> <a href="#">Test </a> </li></ul> </li><li class="dropdown timeit" data-start=" 2019-04-04 00:00"> <a href=""> <span>4. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">Test </a> </li><li role="presentation" class="divider"> </li><li> <a href="#">Test </a> </li></ul> </li><li class="dropdown timeit" data-start=" 2019-04-04 00:00"> <a href=""> <span>5. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">Test </a> </li><li role="presentation" class="divider"> </li><li> <a href="#">Test </a> </li></ul> </li><li class="dropdown timeit" data-start=" 2019-04-04 00:00"> <a href=""> <span>6. </span>Módulo </a> <ul class="dropdown-menu"> <li> <a href="#">Test </a> </li><li role="presentation" class="divider"> </li><li> <a href="#">Test </a> </li></ul> </li></ul> </nav></div>');
+
+// Menu numero 2 -----
+
+// footer curso EPOC-astrazeneca #1
+$('#footer').html('<div class="span5"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/EPOC-astrazeneca/images/svg/logo-ueb.svg" alt="Universidad El Bosque" /></div><div class="span1"></div><div class="span5"><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/EPOC-astrazeneca/images/svg/logo-takeda.svg" alt="Takeda" /></div>');
+
+// footer curso EPOC-astrazeneca #2
+$('#footer-2').html('<div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/EPOC-astrazeneca/images/svg/logo-takeda-oncology.svg" alt="Takeda Oncology" /></div>');
+
+
+//
+// barras progreso
+//
+// progreso 10%
+$('#progreso10').html('<div class="progress progress-striped active"><div class="bar" style="width: 10%;"></div></div>');
+// progreso 20%
+$('#progreso20').html('<div class="progress progress-striped active"><div class="bar" style="width: 20%;"></div></div>');
+// progreso 30%
+$('#progreso30').html('<div class="progress progress-striped active"><div class="bar" style="width: 30%;"></div></div>');
+// progreso 40%
+$('#progreso40').html('<div class="progress progress-striped active"><div class="bar" style="width: 40%;"></div></div>');
+// progreso 50%
+$('#progreso50').html('<div class="progress progress-striped active"><div class="bar" style="width: 50%;"></div></div>');
+// progreso 60%
+$('#progreso60').html('<div class="progress progress-striped active"><div class="bar" style="width: 60%;"></div></div>');
+// progreso 70%
+$('#progreso70').html('<div class="progress progress-striped active"><div class="bar" style="width: 70%;"></div></div>');
+// progreso 80%
+$('#progreso80').html('<div class="progress progress-striped active"><div class="bar" style="width: 80%;"></div></div>');
+// progreso 90%
+$('#progreso90').html('<div class="progress progress-striped active"><div class="bar" style="width: 90%;"></div></div>');
+// progreso 100%
+$('#progreso100').html('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div>');
+
+//
+// elementos modulo 0
+//
+// banner mod 0
+$('#banner-mod-0').html('<div class="span6 txt-banner"><h1>Diplomado<br><span>Expertos en Lípidos</span></h1></div>');
+
+
+//
+// elementos modulo 1
+
+// material mod 1
+$('#material-mod-1').html('<!--<div class="span12"> <h2><span>Material</span> del módulo en progreso</h2></div><ul class="thumbnails span12"> <li class="span3"> <div class="thumbnail"> <div class="caption"> <p>Spicy jalapeno bacon ipsum dolor amet cow tail short loin strip steak swine kielbasa pancetta beef ribs pork frankfurter venison. Pork belly buffalo capicola, picanha short ribs turkey porchetta pig alcatra venison ground round.</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=250703" class="btn btn-primary" >Ver Más </a> </p></div></div></li><li class="span3"> <div class="thumbnail"> <div class="caption"> <p>Spicy jalapeno bacon ipsum dolor amet cow tail short loin strip steak swine kielbasa pancetta beef ribs pork frankfurter venison. Pork belly buffalo capicola, picanha short ribs turkey porchetta pig alcatra venison ground round.</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/page/view.php?id=250704" class="btn btn-primary" >Ver Más </a> </p></div></div></li></ul>-->');
