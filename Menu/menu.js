0b57234a96f66fd604345a42beae959e883ee10c
Unknown <seguragabriel@unbosque.edu.co>
2019-06-27 12:07:12 -0500

    EPOC 27 June (wip)

---
+++ Menu/menu.js
@@ -0,0 +1,50 @@
+// banner mod 0
+$('#banner-0').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/banners/banner-home.jpg" alt="Diplomado imagen"/>');
+
+// banner mod 1
+$('#banner-1').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/banners/banner-home.jpg" alt="Diplomado imagen"/>');
+
+// banner mod 2
+$('#banner-2').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/banners/banner-home.jpg" alt="Diplomado imagen"/>');
+
+// banner mod 3
+$('#banner-3').html('<img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/banners/banner-home.jpg" alt="Diplomado imagen"/>');
+
+// Menu copy
+// <div id="menu_curso"> <div id="menuanchor-top"> <nav> <div class="menu-toggle"> <i class="fa fa-bars"> </i> Menú </div><ul> <li class="dropdown timeit" data-start="2018-07-27 00:00"> <a href="#"> Inicio </a> <ul class="dropdown-menu"> <li> <a href="https://ubosquemoodle.unbosque.edu.co/course/view.php?id=4343">INICIO </a> </li></ul> </li><li class="dropdown timeit" data-start="2018-07-27 00:00"> <a href="#"> Bienvenida </a> <ul class="dropdown-menu"> <li> <a href="#">CARTA BIENVENIDA </a> </li></ul> </li><li class="dropdown timeit" data-start="2018-07-27 00:00"> <a href="#"> Módulo 1 </a> <ul class="dropdown-menu"> <li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9016">DEFINICIÓN </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9017">ETIOLOGÍA </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9018">FACTORES DE RIESGO </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9019">FACTORES DEL HUÉSPED </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9020">FISIOLOGÍA Y FISIOPATOLOGÍA </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9021">PROMOCIÓN DE LA SALUD </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9022">GLOSARIO </a> </li><li> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9023">REFERENCIAS </a> </li><li> <a href="#">CASO CLÍNICO </a> </li><li> <a href="#">EVALUACIÓN </a> </li></ul> </li><li class="dropdown timeit" data-start="2018-07-27 00:00"> <a href="#"> Módulo 2 </a> <ul class="dropdown-menu"> <li> <a href="#">CASO CLÍNICO </a> </li><li> <a href="#">EVALUACIÓN </a> </li></ul> </li><li class="dropdown timeit" data-start="2018-07-27 00:00"> <a href="#"> Módulo 3 </a> <ul class="dropdown-menu"> <li> <a href="#">CASO CLÍNICO </a> </li><li> <a href="#">EVALUACIÓN </a> </li></ul> </li></ul> </nav> </div></div>
+
+
+// footer curso amgen #1
+$('#footer').html('<div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/svg/logo-ueb.svg" alt="Universidad El Bosque - Especialización en Medicina Familiar"/></div><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/svg/logo-AZ.svg" alt="Astrazeneca"/></div><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/svg/logo-BI.svg" alt="Boeringer Ingelheim"/></div><div><img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/svg/logo-novartis.svg" alt="Novartis PHARMACEUTICALS"/></div>');
+
+
+//
+// barras progreso
+//
+// progreso 10%
+$('#progreso10').html('<div class="progress progress-striped active"><div class="bar" style="width: 10%;"></div></div>');
+// progreso 20%
+$('#progreso20').html('<div class="progress progress-striped active"><div class="bar" style="width: 20%;"></div></div>');
+// progreso 30%
+$('#progreso30').html('<div class="progress progress-striped active"><div class="bar" style="width: 30%;"></div></div>');
+// progreso 40%
+$('#progreso40').html('<div class="progress progress-striped active"><div class="bar" style="width: 40%;"></div></div>');
+// progreso 50%
+$('#progreso50').html('<div class="progress progress-striped active"><div class="bar" style="width: 50%;"></div></div>');
+// progreso 60%
+$('#progreso60').html('<div class="progress progress-striped active"><div class="bar" style="width: 60%;"></div></div>');
+// progreso 70%
+$('#progreso70').html('<div class="progress progress-striped active"><div class="bar" style="width: 70%;"></div></div>');
+// progreso 80%
+$('#progreso80').html('<div class="progress progress-striped active"><div class="bar" style="width: 80%;"></div></div>');
+// progreso 90%
+$('#progreso90').html('<div class="progress progress-striped active"><div class="bar" style="width: 90%;"></div></div>');
+// progreso 100%
+$('#progreso100').html('<div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div>');
+
+
+// material mod 1
+$('#material-mod-1').html('<div> <h2>Material</h2></div><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/mod-1/pdf01.jpg"> <div class="caption"> <h3>Presentación PDF </h3> <p>Manejo de EPOC en atención primaria - Módulo 1 prevención primaria</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301266&chapterid=9024" class="btn btn-primary">Ver Presentación </a> </p></div></div></li></ul>');
+
+// material mod 2
+$('#material-mod-2').html('<div> <h2>Material</h2></div><ul class="thumbnails"> <li class="span4"> <div class="thumbnail"> <img src="https://ubosquemoodle.unbosque.edu.co/diseno/cursos/nuevo-EPOC/images/mod-2/pdf01.jpg"> <div class="caption"> <h3>Presentación PDF </h3> <p>Manejo de EPOC en atención primaria - Módulo 2 prevención secundaria</p><p> <a href="https://ubosquemoodle.unbosque.edu.co/mod/book/view.php?id=301274&chapterid=9036" class="btn btn-primary">Ver Presentación </a> </p></div></div></li></ul>');
